2a4c8c96c4818c7209c9b8d8b81c1fb139414479
Ciortan Madalina <ciortan.madalina@intrum.com>
2023-06-05 14:47:35 +0200

    added comments to both python script and unit tests

--- test/test_titanic_model.py
+++ test/test_titanic_model.py
@@ -8,31 +8,57 @@ import train_utils
 from train_utils import load_data, preprocess_data, prepare_data_for_training, prepare_test_data, train_model, make_predictions  # replace 'your_script' with the name of your script
 
 class TestTitanicModel(unittest.TestCase):
+    """
+    A class for unit-testing function in the train_utils.py file
+    Args:
+        unittest.TestCase this allows the new class to inherit
+        from the unittest module
+    """
 
     def test_load_data(self):
+        """
+        Test case for the `load_data` function.
+        Ensure the function returns two pandas DataFrames.
+        """
         train, test = load_data('train.csv', 'test.csv')
         self.assertIsInstance(train, pd.DataFrame)
         self.assertIsInstance(test, pd.DataFrame)
 
     def test_preprocess_data(self):
+        """
+        Test case for the `preprocess_data` function.
+        Ensure the function preprocesses the data correctly.
+        """
         train, test = load_data('train.csv', 'test.csv')
         train, test = preprocess_data(train, test)
         self.assertFalse(train.isnull().values.any())
         self.assertFalse(test.drop("Survived", axis = 1).isnull().values.any())
 
     def test_prepare_data_for_training(self):
+        """
+        Test case for the `prepare_data_for_training` function.
+        Ensure the function prepares the data for training correctly.
+        """
         train, test = load_data('train.csv', 'test.csv')
         train, test = preprocess_data(train, test)
         X_train, y_train = prepare_data_for_training(train)
         self.assertEqual(X_train.shape[0], y_train.shape[0])
 
     def test_prepare_test_data(self):
+        """
+        Test case for the `prepare_test_data` function.
+        Ensure the function prepares the test data correctly.
+        """
         train, test = load_data('train.csv', 'test.csv')
         train, test = preprocess_data(train, test)
         X_test = prepare_test_data(test)
         self.assertIsInstance(X_test, pd.DataFrame)
 
     def test_train_model(self):
+        """
+        Test case for the `train_model` function.
+        Ensure the function trains the model correctly.
+        """
         train, test = load_data('train.csv', 'test.csv')
         train, test = preprocess_data(train, test)
         X_train, y_train = prepare_data_for_training(train)
@@ -40,6 +66,10 @@ class TestTitanicModel(unittest.TestCase):
         self.assertIsNotNone(model)
 
     def test_make_predictions(self):
+        """
+        Test case for the `make_predictions` function.
+        Ensure the function makes predictions correctly.
+        """
         train, test = load_data('train.csv', 'test.csv')
         train, test = preprocess_data(train, test)
         X_train, y_train = prepare_data_for_training(train)
--- train_utils.py
+++ train_utils.py
@@ -5,12 +5,34 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 
 def load_data(train_path, test_path):
+    """
+    Load train and test datasets from CSV files.
+
+    :param train_path: The file path for the training dataset.
+    :type train_path: str
+    :param test_path: The file path for the test dataset.
+    :type test_path: str
+    :returns: The training and test datasets.
+    :rtype: tuple of pandas.DataFrame
+    """
     # Load train and test datasets
     train = pd.read_csv(train_path)
     test = pd.read_csv(test_path)
     return train, test
 
 def preprocess_data(train, test):
+    """
+    Preprocess the training and test datasets.
+    The preprocessing steps include filling missing values, dropping unnecessary columns,
+    and encoding categorical variables.
+
+    :param train: The training dataset.
+    :type train: pandas.DataFrame
+    :param test: The test dataset.
+    :type test: pandas.DataFrame
+    :returns: The preprocessed training and test datasets.
+    :rtype: tuple of pandas.DataFrame
+    """
     # Combine train and test for preprocessing
     data = pd.concat([train, test], sort=False)
 
@@ -34,23 +56,60 @@ def preprocess_data(train, test):
     return train, test
 
 def prepare_data_for_training(train):
+    """
+    Prepare the training data for model training.
+
+    :param train: The preprocessed training dataset.
+    :type train: pandas.DataFrame
+    :returns: The feature matrix and the target array for model training.
+    :rtype: tuple of pandas.DataFrame and pandas.Series
+    """
     # Prepare data for training the model
     y_train = train['Survived']
     X_train = train.drop('Survived', axis=1)
     return X_train, y_train
 
 def prepare_test_data(test):
+    """
+    Prepare the test data for making predictions.
+
+    :param test: The preprocessed test dataset.
+    :type test: pandas.DataFrame
+    :returns: The feature matrix for the test dataset.
+    :rtype: pandas.DataFrame
+    """
     # Prepare test data (this doesn't include 'Survived' column)
     X_test = test.drop('Survived', axis=1)
     return X_test
 
 def train_model(X_train, y_train):
+    """
+    Train a Gaussian Naive Bayes model.
+
+    :param X_train: The feature matrix for the training dataset.
+    :type X_train: pandas.DataFrame
+    :param y_train: The target array for the training dataset.
+    :type y_train: pandas.Series
+    :returns: The trained model.
+    :rtype: sklearn.naive_bayes.GaussianNB
+    """
     # Create a model and train it
     model = GaussianNB()
     model.fit(X_train, y_train)
     return model
 
 def make_predictions(model, X_test):
+    """
+    Make predictions on the test data using the```python
+trained model.
+
+    :param model: The trained Gaussian Naive Bayes model.
+    :type model: sklearn.naive_bayes.GaussianNB
+    :param X_test: The feature matrix for the test dataset.
+    :type X_test: pandas.DataFrame
+    :returns: The predicted outcomes for the test dataset.
+    :rtyp
+    """
     # Predict the survival for test data
     predictions = model.predict(X_test)
     return predictions
